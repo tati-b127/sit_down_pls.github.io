a93ea3ef078504244821960aa8169ace45fad7e9
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2023-01-04 17:52:40 +0300

    Delete src directory

--- src/js/tooltip.js
+++
@@ -1,7 +0,0 @@
-tippy('#tooltip', {
-    content: 'Реплицированные с зарубежных источников, исследования формируют глобальную сеть.',
-    placement: 'top',
-    animation: 'scale-extreme',
-    // theme: 'translucent',
-    // trigger: 'focus',
-  });
